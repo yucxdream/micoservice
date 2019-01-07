deb0414492328e960e15d2e4c18b97c8dd68495e
yucx <yucx@visionet.com.cn>
2019-01-07 11:16:27 +0800

    dev1测试

--- micoservice-feign/src/main/java/com/exception/GlobalExceptionHandler.java
+++ micoservice-feign/src/main/java/com/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-
-
+    public static void main(String[] args) {
+        System.out.println("dev1");
+    }
 }
